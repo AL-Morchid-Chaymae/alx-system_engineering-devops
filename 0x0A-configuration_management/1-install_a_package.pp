4adb17a5a514ef079b828c341991ea272df8749f
AL-Morchid-Chaymae <almorchidchaymae1@gmail.com>
2024-03-22 17:48:15 -0700

    task1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,14 @@
-# Install puppet-lint
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+# Install Flask 2.1.0 and update Werkzeug
+package { 'python3':
+  ensure => installed,
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
